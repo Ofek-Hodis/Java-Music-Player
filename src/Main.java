5f9d7f8fce58ba8d59125a37fb3426260188c107
Ofek-Hodis <ofkhod@gmail.com>
2026-01-17 12:34:04 +0100

    Added volume changing and fixed minor bugs

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -18,14 +19,21 @@ public class Main {
             audioStream = AudioSystem.getAudioInputStream(file);
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
+
+            float decibels = -20; // Variable to keep track of initial (and all other) decibel change
+            FloatControl gainControl =
+                    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(decibels); // Reduce volume initially to avoid overly loud sound
+
             String response = "";
 
             while (!response.equals("Q")) {
-                System.out.println("P = play");
-                System.out.println("S = stop");
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
                 System.out.println("R = Restart");
                 System.out.println("C = Change song");
-                System.out.println("Q = quit");
+                System.out.println("V = Volume control");
+                System.out.println("Q = Quit");
                 response = scanner.next().toUpperCase();
 
                 switch (response) {
@@ -39,6 +47,9 @@ public class Main {
                         audioStream = AudioSystem.getAudioInputStream(file);
                         clip.close(); // Closing the clip before re-opening to allow rechoosing the same song
                         clip.open(audioStream);
+                    }
+                    case "V" -> {
+                        decibels = setVolume(decibels, scanner, clip);
                     }
                     case "Q" -> clip.close();
                     default -> System.out.println("Invalid choice");
@@ -88,8 +99,8 @@ public class Main {
         switch (choice) {
             case 1 -> filePath = "music\\chinese-lunar-new-year.wav";
             case 2 -> filePath = "music\\jazz-music.wav";
-            case 3 -> filePath = "music\\latino-pop.wav";
-            case 4 -> filePath = "music\\modern-jazz.wav";
+            case 3 -> filePath = "music\\modern-jazz.wav";
+            case 4 -> filePath = "music\\latino-pop.wav";
             case 5 -> filePath = "music\\piano-classical-music-1.wav";
             case 6 -> filePath = "music\\piano-classical-music-2.wav";
             case 7 -> filePath = "music\\relax-chill-house-music.wav";
@@ -99,5 +110,32 @@ public class Main {
             }
         }
         return filePath;
+    }
+
+    static float setVolume(float decibels ,Scanner scanner, Clip clip){
+        System.out.println("Current change in decibels is " + decibels + ". Possible range is -80.0 6.0 DB.");
+        System.out.print("Please enter the decibel change you would like to implement(float): ");
+        try{
+            float change = scanner.nextFloat();
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(decibels + change);
+            decibels += change; // Saving the changes to decibels only after verifying the change was possible
+        }
+        catch(InputMismatchException e) {
+            System.out.println("The value you entered is invalid, it must be a float");
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("Gain value out of range (-80.0 to 6.0 dB)");
+        }
+        // A safety net catch clause that gets the error name and prints it to user
+        catch (Exception e){
+            String errorName = e.getClass().getSimpleName();
+            System.out.println("Unexpected error '" + errorName + "': " + e.getMessage());
+        }
+        finally{
+            scanner.nextLine(); //Clearing up the input buffer to avoid future input mistakes
+        }
+
+        return decibels;
     }
 }
